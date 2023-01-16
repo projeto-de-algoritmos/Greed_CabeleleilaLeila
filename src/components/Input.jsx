264ee92392fc14d729cf4ac2b99feadcab02fe5b
João Victor Batista <jvtexbat@hotmail.com>
2023-01-16 17:12:06 -0300

    Create input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,28 @@
+import React, { useState, useEffect } from "react";
+
+export const Input = ({
+    label,
+    placeholder,
+    updateValue,
+    mask = (value) => value
+}) => {
+    const [value, setValue] = useState('');
+
+    useEffect(() => {
+        updateValue(value);
+    }, [value])
+
+    return (
+        <div className="w-full flex flex-col items-start justify-center gap-2">
+            <p className="font-medium text-base text-pink-600">
+                {label}
+            </p>
+            <input
+                className="border-2 border-violet-900 rounded p-2 placeholder-opacity-50 text-slate-900 font-medium text-lg font-sans"
+                placeholder={placeholder}
+                value={value}
+                onChange={e => setValue(mask(e.target.value))}
+            />
+        </div>
+    )
+}
