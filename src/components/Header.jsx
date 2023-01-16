a5c3a56061bd024cd04b4cd8df22821e77e9677f
João Victor Batista <jvtexbat@hotmail.com>
2023-01-16 17:12:33 -0300

    Create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Header = () => {
+    return (
+        <div
+            className="bg-purple-900 w-full flex items-center justify-start p-6"
+        >
+            <h1 className="text-white font-semibold text-2xl">LazyStudent</h1>
+        </div>
+    )
+}
