7b0d78df472dc2a57fa01e2e5ec472f053353c65
João Victor Batista <jvtexbat@hotmail.com>
2023-01-16 21:39:29 -0300

    Add screenshots and README

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Bilbo Bolseiro
+# Lazy Student
 
 **Número da Lista**: 18<br>
 **Conteúdo da Disciplina**: Greed<br>
@@ -11,18 +11,44 @@
 
 
 ## Sobre 
+Visto que muitos alunos da FGA possuem o hábito de realizar suas atividades universitárias de última hora, projeto Lazy Student procura auxiliar esses
+alunos a organizarem suas agendas. O projeto apresenta um algoritmo inspirado no ambicioso *Job Scheduling* com algumas modificações: são informadas uma lista de tarefas a serem realizadas, cada uma com nome, data de entrega (*deadline*) e nível de dificuldade (expresso em dias de execução) e retorna um vetor contendo qual atividade será realizada em qual dia, sempre adiando o máximo possível.
 
+![image0]('src/assets/example.png')
+
+### Requisitos 
+- *npm* ou *yarn*
+- node v16
+
+
+### Instalação
+Para instalar o projeto, bastar seguir os seguintes comandos:
+```
+$ git clone https://github.com/projeto-de-algoritmos/Greed_.git
+```
+Em seguida é preciso mudar para a branch farfetcher utilizando o seguinte comando:
+```
+$ git checkout lazy-student
+```
+E instalar o projeto com o comando:
+```
+$ npm install
+```
+
+Para inicializar o projeto digite o comando:
+```
+$ npm run dev
+```
+
+ou 
+
+```
+$ yarn dev
+```
 
 ## Screenshots
-Adicione 3 ou mais screenshots do projeto em funcionamento.
 
-## Instalação 
-**Linguagem**: JavaScript<br>
-**Framework**: React<br>
-Descreva os pré-requisitos para rodar o seu projeto e os comandos necessários.
+![image1]('src/assets/Screenshot1.png')
+![image2]('src/assets/Screenshot2.png')
+![image3]('src/assets/Screenshot3.png')
 
-## Uso 
-Explique como usar seu projeto caso haja algum passo a passo após o comando de execução.
-
-## Outros 
-Quaisquer outras informações sobre seu projeto podem ser descritas abaixo.
--- src/utils/lazyScheduler.js
+++ src/utils/lazyScheduler.js
@@ -1,26 +1,3 @@
-// const jobs = [
-//     {
-//         id: 'a',
-//         deadline: 1,
-//         diff: 1
-//     },
-//     {
-//         id: 'c',
-//         deadline: 3,
-//         diff: 1
-//     },
-//     {
-//         id: 'd',
-//         deadline: 6,
-//         diff: 2
-//     },
-//     {
-//         id: 'b',
-//         deadline: 3,
-//         diff: 1
-//     },
-// ]
-
 const sortJobs = (jobA, jobB) => {
     if (jobA.deadline > jobB.deadline) return -1;
     else if (jobA.deadline < jobB.deadline) return 1;
@@ -32,15 +9,15 @@ export const scheduleJobs = (list) => {
     const maxDealine = list[0].deadline;
     const result = Array.from({ length: maxDealine }, () => null);
 
-    for (let i = maxDealine - 1; i >= 0; i--) {
+    for (let i = maxDealine - 1; i >= 0; i--) { // vou para o prox dia
         let j = i;
         const jobsForDay = [...list.filter(job => { if (job.deadline - 1 == i) return job; })];
 
-        if (jobsForDay.length) {
-            while (result[j] && j >= 0) j -= 1;
+        if (jobsForDay.length) { // existem tarefas para esse dia???
+            while (result[j] && j >= 0) j -= 1; // o dia está livre?? Caso não esteja, percorrer até encontrar dia livre
 
             if (j >= 0) {
-                jobsForDay.forEach(element => {
+                jobsForDay.forEach(element => {  // para cada tarefa para o dia, preencher 'diff' num de dias com a tarefa
                     while (result[j] && j >= 0) j -= 1;
 
                     if (j >= 0) {
@@ -56,5 +33,34 @@ export const scheduleJobs = (list) => {
         }
     }
 
-    return result;
+    return result; // para testar, trocar por 'console.log(result)'
 }
+
+/* para testar, 
+ - descomentar variavel 'jobs' abaixo
+ const jobs = [
+    {
+        id: 'a',
+        deadline: 1,
+        diff: 1
+    },
+    {
+        id: 'c',
+        deadline: 3,
+        diff: 1
+    },
+    {
+        id: 'd',
+        deadline: 6,
+        diff: 2
+    },
+    {
+        id: 'b',
+        deadline: 3,
+        diff: 1
+    },
+]
+
+ - trocar por 'console.log(result)' na linha 36
+ - adicionar 'scheduleJobs(jobs)' ao final do arquivo;
+*/
